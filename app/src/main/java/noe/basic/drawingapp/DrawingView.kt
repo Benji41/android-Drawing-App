ef090815d2bdf544ef744d841da7ce2ebe922502
Noe Benjamin Reynoso <nreynoso@geniuscode.mx>
2022-07-25 21:15:59 -0700

    uso de cases internal inner, permite que la clase hija se visible a la padre y vicebersa pero no fuera de la clase padre, ambas clases comparten variables y metodos entre si.

---
+++ app/src/main/java/noe/basic/drawingapp/DrawingView.kt
@@ -0,0 +1,15 @@
+package noe.basic.drawingapp
+
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class DrawingView(context: Context,attrs: AttributeSet) : View( context,attrs ) {
+    private val mDrawPath : CustomPath? = null
+    internal inner class CustomPath {
+
+    }
+}
+
+
